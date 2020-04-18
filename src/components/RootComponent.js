214dd6603fe1a09173b0783942e81da30ddcdaac
Sergio Andres Florez <sergio.florez@grability.com>
2020-04-18 15:30:25 -0500

    add ui components to landing

--- src/components/RootComponent.js
+++ src/components/RootComponent.js
@@ -2,24 +2,13 @@ import React from 'react';
 
 import { HashRouter, Switch, Route } from "react-router-dom";
 
-import App from "./App";
 import Home from "./screens/home";
-import About from "./screens/about";
-
-import Header from "./ui/header/Header";
-
-import * as Tags from "./RootComponent.styled";
 
 const RootComponent = ({location, context}) => (
   <HashRouter location={location} context={context}>
-    <Tags.MainContainer>
-      <Header />
       <Switch>
-        <Route exact path="/" component={App} />
         <Route exact path="/dashboard" component={Home} />
-        <Route exact path="/about" component={About} />
       </Switch>
-    </Tags.MainContainer>
   </HashRouter>
 );
 
--- src/components/screens/home.js
+++ src/components/screens/home.js
@@ -3,19 +3,6 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>Home</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
