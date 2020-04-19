7c9b5c40e829eb343d0ef20365b7fb4c4fe8451b
Sergio Andres Florez <sergio.florez@grability.com>
2020-04-18 21:34:23 -0500

    grid resposive

--- src/components/ui/character-grid/CharacterGrid.styled.js
+++ src/components/ui/character-grid/CharacterGrid.styled.js
@@ -1,9 +1,22 @@
 import styled from 'styled-components';
 
-export const Title = styled.div``;
+export const Title = styled.div`
+@media (min-width: 320px) and (max-width: 480px) {
+  margin-left: 20px;
+}
+@media (min-width: 481px) and (max-width: 1024px) {
+  margin-left: 20px;
+}
+`;
 
 export const CharacterCardContainer = styled.div`
 display: flex;
 justify-content: space-between;
 flex-wrap: wrap;
+@media (min-width: 320px) and (max-width: 480px) {
+  margin: 0 20px;
+}
+@media (min-width: 481px) and (max-width: 1024px) {
+  margin: 0 20px;
+}
 `;
