9523cd24f29b67204aae4ecf056529ca1280f304
Sergio Andres Florez <sergio.florez@grability.com>
2020-04-21 20:58:43 -0500

    add loader

--- src/components/RootComponent.js
+++ src/components/RootComponent.js
@@ -3,9 +3,12 @@ import { HashRouter, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import App from "../lib/app";
-
 import Home from "./screens/Home";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+import { MainContainer } from './RootComponent.styled';
 
 const RootComponent = ({location, context}) => {
   const [isReady, setIsReady] = useState(false);
@@ -17,7 +20,11 @@ const RootComponent = ({location, context}) => {
   }, []);
 
   if (!isReady) {
-    return <div>Loading</div>;
+    return (
+      <MainContainer>
+        <FontAwesomeIcon color="white" icon={faSpinner} size="2x" spin={true}/>
+      </MainContainer>
+    );
   }
 
   return (
--- src/components/RootComponent.styled.js
+++ src/components/RootComponent.styled.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
-  background-image: linear-gradient(to bottom right, withe, blue);
+  background: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `;
